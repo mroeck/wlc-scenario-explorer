1d00c8cf20883c2cdd85dcf5cb38d9176d4b3785
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-05-26 19:17:31 +0200

    backend Dockerfile dev and prod functionnal

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 if [ ${ENV} = "DEV" ]; then
-    # poetry run python app.py
-    # flask run --host "0.0.0.0"
     flask --app app run --host "0.0.0.0"
 else
-    # temp:
-    poetry run python app.py
+    poetry run gunicorn --config gunicorn_config.py app:app
 fi
