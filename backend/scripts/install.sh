d36907721ae158084a5099e707ce4e5c3bcc143a
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-05-27 18:23:23 +0200

    refactor: docker backend

--- backend/scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ${ENV} = "DEV" ]; then
-    poetry install
-else
-    poetry install --only main
-fi
--- backend/scripts/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ${ENV} = "DEV" ]; then
-    flask --app app run --host "0.0.0.0"
-else
-    poetry run gunicorn --config gunicorn_config.py app:app
-fi
